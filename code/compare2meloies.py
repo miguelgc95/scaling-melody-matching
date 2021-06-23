379de2cf6adc58faf57325fe3123513914a21438
miguelgc95 <miguelgc95@gmail.com>
2021-06-23 09:10:19 +0200

    quick refactor of compare2melodies

--- code/compare2meloies.py
+++ code/compare2meloies.py
@@ -5,54 +5,17 @@ import os
 
 BASE_PATH = os.getcwd()
 
-# QUERY PRE-TREATMENT
-query_path = BASE_PATH + '/DB_files/11-D_TPabon.csv'
+query_path = BASE_PATH + '/DB_files/deblas/11-D_TPabon.csv'
 
-query = pd.read_csv(query_path, names=["inicio", "duración",
-                                       "tono", "nidea"]).drop([0], axis=0)
+Q = modules.auxiliar_functions.create_query(query_path)
 
-time_where_song_starts = float(query.iloc[0, 0])
+reference_path = BASE_PATH + '/DB_files/deblas/01-D_AMairena.csv'
 
-# cambiar formato de string a float y desplazar la cancion para forzar que empiece en cero
-for i in range(len(query)):
-    if type(query.iloc[i, 0]) == str:
-        query.iloc[i, 0] = float(query.iloc[i, 0])
-    if type(query.iloc[i, 1]) == str:
-        query.iloc[i, 1] = float(query.iloc[i, 1])
-    if type(query.iloc[i, 2]) == str:
-        query.iloc[i, 2] = float(query.iloc[i, 2])
-    query.iloc[i, 0] = (query.iloc[i, 0]-time_where_song_starts)
-
-Q = auxiliar_functions.prepare_melody(query)
-
-
-# REFERENCE PRE-TREATMENT
-reference_path = BASE_PATH + '/DB_files/01-D_AMairena.csv'
-# reference_path = BASE_PATH + '/DB_files/02-D_ChanoLobato.csv'
-# reference_path = BASE_PATH + '/DB_files/03-D_Chocolate.csv'
-# reference_path = BASE_PATH + '/DB_files/04-D_JAlmaden.csv'
-# reference_path = BASE_PATH + '/DB_files/05-D_JHeredia.csv'
-# reference_path = BASE_PATH + '/DB_files/09-D_PdeLucia.csv'
-
-reference = pd.read_csv(reference_path, names=["inicio", "duración",
-                                               "tono", "nidea"]).drop([0], axis=0)
-
-time_where_song_starts = float(reference.iloc[0, 0])
-
-# cambiar formato de string a float y desplazar la cancion para forzar que empiece en cero
-for i in range(len(reference)):
-    if type(reference.iloc[i, 0]) == str:
-        reference.iloc[i, 0] = float(reference.iloc[i, 0])
-    if type(reference.iloc[i, 1]) == str:
-        reference.iloc[i, 1] = float(reference.iloc[i, 1])
-    if type(reference.iloc[i, 2]) == str:
-        reference.iloc[i, 2] = float(reference.iloc[i, 2])
-    reference.iloc[i, 0] = (reference.iloc[i, 0]-time_where_song_starts)
-
-R = auxiliar_functions.prepare_melody(reference)
+# We can use the function 'create_query' to create the reference as long as it just create a valid melody from a .csv file
+R = modules.auxiliar_functions.create_query(reference_path)
 
 # aplicamos el algoritmo
-result = []
+
 if Q[-1][1] > R[-1][1]:  # en caso de que Q sea más grande que R, intercambiamos sus papeles para poder aplicar el algoritmo y escalamos R en vez de Q
     P = Q[:]
     Q = R[:]
@@ -60,15 +23,16 @@ if Q[-1][1] > R[-1][1]:  # en caso de que Q sea más grande que R, intercambiamo
 
 maxeps = (R[-1][1]-Q[-1][1])/len(Q)
 Q[-1][1] = R[-1][1]
-(areainicial, h11, h22, h33) = area_inicial.initial_area(R, Q)
-q = calculoeventos.calculaeventos_main(R, Q, maxeps)
-h = auxiliar_functions.heap(q)
-areamin, epsmin, ayuda = actualizacionarea.actualizar(
+(areainicial, h11, h22, h33) = modules.area_inicial.initial_area(R, Q)
+q = modules.calculoeventos.calculaeventos_main(R, Q, maxeps)
+h = modules.auxiliar_functions.heap(q)
+areamin, epsmin, ayuda = modules.actualizacionarea.actualizar(
     h, q, areainicial, h11, h22, h33, maxeps)
-# guardamos en una lista la info de cada una de las referencias
-result.append([areamin, epsmin])
+
+result = [areamin, epsmin]
+
 # auxiliar_functions.dibuja(R,Q,0)
-areas = auxiliar_functions.comprueba_area(R, Q, q)
+areas = modules.auxiliar_functions.comprueba_area(R, Q, q)
 
 print(result)
 
